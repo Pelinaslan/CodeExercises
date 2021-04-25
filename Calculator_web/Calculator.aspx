83e7341f86b08a9598e6b0260cce1a5a72b1c2b6
Pelinaslan <44268597+Pelinaslan@users.noreply.github.com>
2021-04-25 21:25:36 +0300

    C#

---
+++ Calculator_web/Calculator.aspx
@@ -0,0 +1,51 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Calculator.aspx.cs" Inherits="HesapMakinesi.Calculator" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Hesap Makinesi</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div >
+            <p>
+            <asp:TextBox ID="TextBox" runat="server" Width="204px" Height="40px"  Font-Bold="true" Font-Size="20px" BorderColor="Black" AutoPostBack="True"></asp:TextBox>
+            <br/>
+            </p>
+
+            <p>
+            <asp:Button ID="Button1" Height="60px" Width="50px" Font-Size="Large" Text-size="100px" runat="server" BorderColor="Blue" Text="7" OnClick="Button1_Click" /> 
+            <asp:Button ID="Button2"  Height="60px" Width="50px" Font-Size="Large" runat="server" BorderColor="Blue" Text="8" OnClick="Button2_Click" /> 
+            <asp:Button ID="Button3"  Height="60px" Width="50px" Font-Size="Large" runat="server" BorderColor="Blue" Text="9" OnClick="Button3_Click" />             
+            <asp:Button ID="Button4"  Height="60px" Width="50px" Font-Size="Large" runat="server" BorderColor="Blue" Text="/" OnClick="Button4_Click" />
+            <br/>            
+            </p>
+
+            <p>
+            <asp:Button ID="Button5" Height="60px" Width="50px" Font-Size="Large" runat="server" BorderColor="Blue" Text="4" OnClick="Button5_Click" /> 
+            <asp:Button ID="Button6"  Height="60px" Width="50px" Font-Size="Large" runat="server" BorderColor="Blue" Text="5" OnClick="Button6_Click" /> 
+            <asp:Button ID="Button7"  Height="60px" Width="50px" Font-Size="Large" runat="server" BorderColor="Blue" Text="6" OnClick="Button7_Click" /> 
+            <asp:Button ID="Button8"  Height="60px" Width="50px" Font-Size="Large" runat="server" BorderColor="Blue" Text="*" OnClick="Button8_Click" />
+            <br/>
+            </p>
+
+            <p>
+            <asp:Button ID="Button9" Height="60px" Width="50px" Font-Size="Large" runat="server" BorderColor="Blue" Text="1" OnClick="Button9_Click" /> 
+            <asp:Button ID="Button10"  Height="60px" Width="50px" Font-Size="Large" runat="server" BorderColor="Blue" Text="2" OnClick="Button10_Click" /> 
+            <asp:Button ID="Button11"  Height="60px" Width="50px" Font-Size="Large" runat="server" BorderColor="Blue" Text="3" OnClick="Button11_Click" /> 
+            <asp:Button ID="Button12"  Height="60px" Width="50px" Font-Size="Large" runat="server" BorderColor="Blue" Text="-" OnClick="Button12_Click" />
+            <br/>
+            </p>
+
+            <p>
+            <asp:Button ID="Button13" Height="60px" Width="50px" Font-Size="Large" runat="server" BorderColor="Blue" Text="0" OnClick="Button13_Click" /> 
+            <asp:Button ID="Button14"  Height="60px" Width="50px" Font-Size="Large" runat="server" BorderColor="Blue" Text="+" OnClick="Button14_Click" /> 
+            <asp:Button ID="Button15" Height="60px" Width="50px" Font-Size="Large" runat="server" BorderColor="Blue" Text="CLC" OnClick="Button15_Click" /> 
+            <asp:Button ID="Button16"  Height="60px" Width="50px" Font-Size="Large" runat="server" BorderColor="Blue" Text="=" OnClick="Button16_Click" />
+            </p>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Calculator_web/Calculator.aspx.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HesapMakinesi
+{
+    public partial class Calculator : System.Web.UI.Page
+    {
+        static string islem;
+        static double sayi1,sayi2;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack) TextBox.Text = "0";// başlangıçta ekrana sıfır yazdıralım.
+        }
+
+        // 1 rakamının bulunduğu buttonun click olayı
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (TextBox.Text == "0")
+                TextBox.Text = "";
+            TextBox.Text += "7";
+        }
+        // 2 rakamının bulunduğu buttonun click olayı
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            if (TextBox.Text == "0")
+                TextBox.Text = "";
+            TextBox.Text += "8";
+        }
+        // 3 rakamının bulunduğu buttonun click olayı
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            if (TextBox.Text == "0")
+                TextBox.Text = "";
+            TextBox.Text += "9";
+        }
+
+        // 4 rakamının bulunduğu buttonun click olayı
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+            if (TextBox.Text == "0")
+                TextBox.Text = "";
+            TextBox.Text += "4";
+        }
+        // 5 rakamının bulunduğu buttonun click olayı
+        protected void Button6_Click(object sender, EventArgs e)
+        {
+            if (TextBox.Text == "0")
+                TextBox.Text = "";
+            TextBox.Text += "5";
+        }
+        // 6 rakamının bulunduğu buttonun click olayı
+        protected void Button7_Click(object sender, EventArgs e)
+        {
+            if (TextBox.Text == "0")
+                TextBox.Text = "";
+            TextBox.Text += "6";
+        }
+
+        // 1 rakamının bulunduğu buttonun click olayı
+        protected void Button9_Click(object sender, EventArgs e)
+        {
+            if (TextBox.Text == "0")
+                TextBox.Text = "";
+            TextBox.Text += "1";
+        }
+        // 2 rakamının bulunduğu buttonun click olayı
+        protected void Button10_Click(object sender, EventArgs e)
+        {
+            if (TextBox.Text == "0")
+                TextBox.Text = "";
+            TextBox.Text += "2";
+        }
+
+        // 3 rakamının bulunduğu buttonun click olayı
+        protected void Button11_Click(object sender, EventArgs e)
+        {
+            if (TextBox.Text == "0")
+                TextBox.Text = "";
+            TextBox.Text += "3";
+        }
+        // 0 rakamının bulunduğu buttonun click olayı
+        protected void Button13_Click(object sender, EventArgs e)
+        {
+            if (TextBox.Text == "0") 
+                TextBox.Text = "0";
+            else TextBox.Text += "0";
+        }
+
+ 
+        // toplama buttonun click olayı
+        protected void Button14_Click(object sender, EventArgs e)
+        {
+            islem = "+";
+            sayi1 = Convert.ToDouble(TextBox.Text);
+            TextBox.Text += "+";
+        }
+     
+        // çıkarma buttonun click olayı
+        protected void Button12_Click(object sender, EventArgs e)
+        {
+            islem = "-";
+            sayi1 = Convert.ToDouble(TextBox.Text);
+            TextBox.Text += "-";
+        }
+
+        // çarpma buttonun click olayı
+        protected void Button8_Click(object sender, EventArgs e)
+        {
+            islem = "*";
+            sayi1 = Convert.ToDouble(TextBox.Text);
+            TextBox.Text += "*";
+        }
+        // bölme buttonun click olayı
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            islem = "/";
+            sayi1 = Convert.ToDouble(TextBox.Text);
+            TextBox.Text += "/";
+        }
+
+        // eşittir buttonun click olayı
+        protected void Button16_Click(object sender, EventArgs e)
+        {
+            string veri = TextBox.Text;
+            string sayi ="";
+
+            for (int i =0 ; i < veri.Length ; i++)
+            {
+                if (veri[i].ToString() == islem)
+                {
+                    for (int j = i+1; j < veri.Length ; j++)
+                    {
+                        sayi += veri[j];
+                    }
+                }
+            }
+
+
+            sayi2 = Convert.ToDouble(sayi);
+            TextBox.Text = Convert.ToString(hesapla());
+
+        }
+
+        public double hesapla()
+        {
+            double sonuc=0;
+
+            if (islem == "+")
+                sonuc = sayi1 + sayi2;
+
+            else if (islem == "-")
+                sonuc = sayi1 - sayi2;
+
+            else if (islem == "*")
+                sonuc = sayi1 * sayi2;
+
+            else if (islem == "/")
+                sonuc = sayi1 / sayi2;
+
+            else
+                sonuc = 0;
+
+            return sonuc;
+
+        }
+
+
+            // ekran temizleme (CLC)
+            protected void Button15_Click(object sender, EventArgs e)
+        {
+            
+            TextBox.Text = "0";
+            
+           
+        }
+    }
+}
